17665154ff47bede1a462f42b4a2d374b76ad139
argentum88 <argentimgipan@gmail.com>
2023-07-16 22:47:12 +0500

    Try CommandLineRunner

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -2,15 +2,21 @@ package tacos;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
 
 @Data
-public class Ingredient {
+public class Ingredient implements Persistable<String> {
     @Id
     private final String id;
 
     private final String name;
 
     private final Type type;
+
+    @Override
+    public boolean isNew() {
+        return true;
+    }
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
